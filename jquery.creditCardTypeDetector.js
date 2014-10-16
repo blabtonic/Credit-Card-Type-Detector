adeb5b804e14ba05489f0f3918f6eef5c770980d
Trystan Hendricks <dolkhound@gmx.com>
2014-10-16 11:51:11 -0400

    Update jquery.creditCardTypeDetector.js

--- jquery.creditCardTypeDetector.js
+++ jquery.creditCardTypeDetector.js
@@ -1,22 +1,4 @@
 /*!
- * jQuery Credit Card Type Detector plugin
- * version 0.9
- * by Christian Reed - http://christianreed.org
- * with support from Nick James - http://nickjames.info
- * for Athletepath - http://athletepath.com
- * https://github.com/christianreed/Credit-Card-Type-Detector
- * Copyright (c) 2012 Christian Reed
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
